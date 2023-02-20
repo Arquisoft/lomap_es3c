d43c05210018ca0609be06ad5d672ae6bf99bcf7
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-02-20 10:46:41 +0100

    Merge con develop Changes to be committed:

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -24,9 +24,9 @@ function App(): JSX.Element {
   return (
     <>
       <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
+        <Welcome message="ASW students" aa="ASW"/>
         <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
+        <EmailForm OnUserListChange={refreshUserList}/>
         <UserList users={users}/>
         <Link href="https://github.com/arquisoft/lomap_0">Source code</Link>
       </Container>
--- webapp/src/components/Welcome.tsx
+++ webapp/src/components/Welcome.tsx
@@ -4,19 +4,19 @@ import logo from '../logo.svg';
 
 type WelcomeProps = {
   message: string;
+  aa: string;
 }
 
 function Welcome(props: WelcomeProps): JSX.Element {
 
   return (
     <Grid container>
-      <Grid item xs={10}>
-        <Box component="h2">Hi, {props.message}</Box>
+      <Grid item xs={9}>
+        <Box component="h2">Hi, {props.message +" " +  props.aa } </Box>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <img src={logo} className="App-logo" alt="logo" />
       </Grid>
-      
     </Grid>
     
   );
