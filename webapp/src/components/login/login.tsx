4e791e0c1fcaa911fc56f41b6edb65012544f610
elpla <elplayerhd@hotmail.es>
2023-02-27 20:03:11 +0100

    Añadiendo redirección a login inrupt y recogida de datos

--- webapp/src/components/login/login.tsx
+++ webapp/src/components/login/login.tsx
@@ -4,6 +4,37 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/esm/Col';
 import Form from 'react-bootstrap/Form';
 import "./login.css";
+import {
+  login,
+  handleIncomingRedirect,
+  onSessionRestore,
+  getDefaultSession,
+  fetch
+} from "@inrupt/solid-client-authn-browser";
+
+async function handleLogin(){
+  //Obtenemos el valor del Identity provider
+  let selectedElement = (document.getElementById("select-idp")) as HTMLSelectElement;
+  let selectedIdp = selectedElement.options[selectedElement.selectedIndex].value; //Obtenemos el valor de la opcion seleccionada mediante el indice seleccionado
+  await login({
+    oidcIssuer: selectedIdp,
+    redirectUrl: window.location.href, //Url a la que nos llevara una vez logeado
+    clientName: "LoMap_es3c"
+  });
+}
+
+handleRedirectAfterLogin();
+
+/*
+Funcion que procesa la informacion de inicio de sesion 
+*/
+async function handleRedirectAfterLogin() {
+  await handleIncomingRedirect(); //Obtiene la informacion de identificacion aportada por el identity provider
+  const session = getDefaultSession();
+  if (session.info.isLoggedIn) {
+    alert("Logeado con exito: " + session.info.webId);
+  }
+}
 
 function LogIn() {
   return (
@@ -12,13 +43,13 @@ function LogIn() {
         <Form>
           <Form.Group controlId="formLogin" className="mb-3">
             <Form.Label className='select-text'>Selecciona un proveedor de identidad</Form.Label>
-            <Form.Select defaultValue="Inrupt">
-              <option>Inrupt</option>
+            <Form.Select defaultValue="Inrupt" id='select-idp'>
+              <option value="https://login.inrupt.com">Inrupt</option>
             </Form.Select>
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <Button type="submit">Iniciar sesión</Button>
+            <Button type="submit" onClick={ ()=> handleLogin()}>Iniciar sesión</Button>
           </Form.Group>
 
         </Form>
