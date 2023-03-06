8e33180438f8f49ff80faf9d0a49e3ce5716c6d4
elpla <elplayerhd@hotmail.es>
2023-03-06 18:59:16 +0100

    Añadiendo primera versión mapa (se muestra y detecta clicks)

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { HomePage } from './components/HomePage/HomePage';
 import { Route, Routes } from "react-router-dom";
 import LogIn from './components/login/login';
+import Map from './components/map/Map';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<HomePage/>} />
         <Route path='/home' element={<HomePage/>} />
         <Route path='/login' element={<LogIn/>} />
+        <Route path='/map' element={<Map/>} />
       </Routes>
     </div>
   );
