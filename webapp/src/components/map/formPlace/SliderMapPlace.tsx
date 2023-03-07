97d2d644fd614c2dbc57b784035049e9f1367f5b
elpla <elplayerhd@hotmail.es>
2023-03-07 19:52:32 +0100

    Añadiendo menú lateral mapa y formulario para añadir punto

---
+++ webapp/src/components/map/formPlace/SliderMapPlace.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+interface Identity {
+    id : string;
+}
+
+const marks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+  {
+    value: 10,
+    label: '10',
+  },
+];
+
+export default function SliderMapPlace(props:Identity) {
+  return (
+    <Box className='m-2' sx={{ width: 'auto' }}>
+      <Slider
+        aria-label="Custom marks"
+        defaultValue={5}
+        step={1}
+        valueLabelDisplay="auto"
+        marks={marks}
+        max={10}
+        id={props.id}
+      />
+    </Box>
+  );
+}
