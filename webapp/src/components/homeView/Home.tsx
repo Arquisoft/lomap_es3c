4b0bf1ff3324ec96200efab3bfab62de2faa9134
Jorge Casatejada Santamarta <UO282294@uniovi.es>
2023-03-27 18:38:26 +0200

    Refactorización pantalla Index
    Se ha pasado todo al login de Inrupt y añadido información. También se han recogido los datos necesarios para que tras realizar el login se puedan guardar en la Base de Datos.

--- webapp/src/components/homeView/Home.tsx
+++ webapp/src/components/homeView/Home.tsx
@@ -4,36 +4,59 @@ import LateralMenu from './LateralMenu';
 import Map from '../map/Map';
 import { Box, styled } from '@mui/material';
 import { Route, Routes } from "react-router-dom";
+import { getDefaultSession, handleIncomingRedirect } from "@inrupt/solid-client-authn-browser";
 
-const IzqBox = styled(Box) ({
-    width: "80%",
-  })
-  
-  const DerBox = styled(Box) ({
-    width: "20%",
-    justifyContent:"right"
-  })
-  
-  const SRoutes = styled(Routes) ({
-      width: "100%"
-  })
-  
-  const Content = styled(Box) ({
-    display: "flex",
-    flexDirection: "row"
-  })
+const IzqBox = styled(Box)({
+  width: "80%",
+})
+
+const DerBox = styled(Box)({
+  width: "20%",
+  justifyContent: "right"
+})
+
+const SRoutes = styled(Routes)({
+  width: "100%"
+})
+
+const Content = styled(Box)({
+  display: "flex",
+  flexDirection: "row"
+})
+
+//Manejar la sesion del proveedor
+async function handleRedirectAfterIdentification() {
+  await handleIncomingRedirect();
+  const session = getDefaultSession();
+  if (session.info.isLoggedIn) {
+    const userWebId = session.info.webId?.split('/profile')[0];
+    const userName = userWebId?.split('//')[1].split('.')[0];
+    const provider = userWebId?.split('//')[1].split('.')[1];
+    //Realizar el POST
+    await fetch("/mi-endpoint", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+          provider: provider,
+          userName: userName,
+          webId: userWebId
+      })
+  });
+  }
+}
 
 export const Home = () => {
+  handleRedirectAfterIdentification();
   return (
     <>
       <TopBar></TopBar>
       <Content>
         <IzqBox>
           <SRoutes>
-            <Route path='/' element={<Map/>}/>
+            <Route path='/' element={<Map />} />
           </SRoutes>
         </IzqBox>
-        <DerBox sx={{display: "flex", justifyContent: "left"}}>
+        <DerBox sx={{ display: "flex", justifyContent: "left" }}>
           <LateralMenu></LateralMenu>
         </DerBox>
       </Content>
--- webapp/src/components/indexView/RegisterProvider.tsx
+++ webapp/src/components/indexView/RegisterProvider.tsx
@@ -1,58 +1,83 @@
 import { NativeSelect, Box, Button, InputLabel, FormControl } from '@mui/material'
 import { useState } from 'react'
-import { login } from "@inrupt/solid-client-authn-browser";
+import { login, getDefaultSession, handleIncomingRedirect } from "@inrupt/solid-client-authn-browser";
 
 
 export const RegisterProvider = () => {
 
     const [provider, setProvider] = useState('');
+    const [userName, setUserName] = useState('');
+    const [webId, setWebId] = useState('');
 
-    const register = async function () {
+    const identification = async function () {
         await login({
             oidcIssuer: provider,
-            redirectUrl: "http://localhost:3000/",//Url a la que nos llevara una vez logeado
+            redirectUrl: "http://localhost:3000/home",//Url a la que nos llevara una vez logeado
             clientName: "LoMap_es3c"
         });
+        console.log("dsafdsf");
+        await handleIncomingRedirect();
+        console.log("wwwww");
+        const session = getDefaultSession();
+        if (session.info.isLoggedIn) {
+            const userWebId = session.info.webId?.split('#')[0];
+            setUserName("");
+            setWebId("");
+        }
+        //Realizar el POST
+        await fetch("/mi-endpoint", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                provider: provider,
+                userName: userName,
+                webId: webId
+            })
+        });
+        //Se puede manejar respuesta del POST con then
     }
 
     return (
-        <Box sx={{
-            marginLeft: "25%", marginRight: "25%", height: "67vh", backgroundColor: 'rgba(25, 118, 210, 1)', border: "solid black 0.2em",
-            paddingTop: "3em", display: "flex", flexDirection: "column"
-        }}>
+            <Box sx={{
+                marginLeft: "25%", marginRight: "25%", height: "67vh", backgroundColor: 'rgba(25, 118, 210, 1)', border: "solid black 0.2em",
+                paddingTop: "3em", display: "flex", flexDirection: "column"
+            }}>
 
-            <Box sx={{ fontSize: "1.3em", color: "black" }}><strong> REGISTRO </strong></Box>
+                <Box sx={{ fontSize: "1.3em", color: "black" }}><strong> IDENTIFÍCATE </strong></Box>
 
-            <Box sx={{ height: "5vh" }}></Box>
+                <Box sx={{ height: "5vh" }}></Box>
 
-            <FormControl required sx={{ marginLeft: "30%", marginRight: "30%", width: "40%" }}>
-                <InputLabel variant="standard" htmlFor="select-native">
-                    Proveedor de identidad
-                </InputLabel>
-                <NativeSelect
-                    defaultValue={""}
-                    required
-                    inputProps={{
-                        name: 'provider',
-                        id: 'select-native',
-                    }}
-                    onChange={(event) => setProvider(event.target.value)}
-                >
-                    <option value="" disabled></option>
-                    <option value={"https://inrupt.net"}>  Inrupt  </option >
-                    <option value={"https://solidcommunity.net/"}>  Solid Project  </option >
-                    <option value={"https://solidweb.org/"}>  Solid Grassroots  </option >
-                    <option value={"https://datapod.igrant.io/"}>  iGrant.io  </option >
-                </NativeSelect>
-            </FormControl>
+                <FormControl required sx={{ marginLeft: "30%", marginRight: "30%", width: "40%" }}>
+                    <InputLabel variant="standard" htmlFor="select-native">
+                        Proveedor de identidad
+                    </InputLabel>
+                    <NativeSelect
+                        defaultValue={""}
+                        required
+                        inputProps={{
+                            name: 'provider',
+                            id: 'select-native',
+                        }}
+                        onChange={(event) => setProvider(event.target.value)}
+                    >
+                        <option value="" disabled></option>
+                        <option value={"https://inrupt.net"}>  Inrupt  </option >
+                        <option value={"https://solidcommunity.net/"}>  Solid Project  </option >
+                        <option value={"https://solidweb.org/"}>  Solid Grassroots  </option >
+                        <option value={"https://datapod.igrant.io/"}>  iGrant.io  </option >
+                    </NativeSelect>
+                </FormControl>
 
-            <Box sx={{ height: "3vh" }}></Box>
+                <input type="hidden" name="userName" value={userName}/>
+                <input type="hidden" name="webId" value={webId}/>
 
-            <Box>
-                <Button variant="outlined" sx={{ color: "black", border: "solid black 0.1em" }} onClick={register}>
-                    Registrar POD
-                </Button>
+                <Box sx={{ height: "3vh" }}></Box>
+
+                <Box>
+                    <Button variant="outlined" sx={{ color: "black", border: "solid black 0.1em" }} onClick={identification}>
+                        Acceder
+                    </Button>
+                </Box>
             </Box>
-        </Box>
     )
 }
