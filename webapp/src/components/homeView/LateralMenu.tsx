2a0b1f254b66b5484ae216337b436592b16ed36b
uo282936 <uo282936@uniovi.es>
2023-04-26 22:01:41 +0200

    Flechas desplegables usabilidad, detalles visuales

--- webapp/src/components/homeView/LateralMenu.tsx
+++ webapp/src/components/homeView/LateralMenu.tsx
@@ -13,6 +13,7 @@ import FriendsList from './FriendsList';
 import {MapInfo } from '../map/Map';
 import { useState } from 'react';
 import MapListAux from './MapListAux';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 export default function LateralMenu(props:MapInfo) {
 
@@ -34,6 +35,7 @@ export default function LateralMenu(props:MapInfo) {
         <Box>
           <MenuItem onClick={handleCollapseMaps}>
             <ListItemText><strong>MIS MAPAS</strong></ListItemText>
+            {openMaps ? <FaChevronDown /> : <FaChevronUp />}
           </MenuItem>
           <Divider sx={{ opacity: 1 }} />
           <Collapse in={openMaps} timeout="auto" unmountOnExit>
@@ -44,6 +46,7 @@ export default function LateralMenu(props:MapInfo) {
         <Box sx={{ borderTop: "solid black 0.25em", paddingTop: "0.5em" }}>
           <MenuItem onClick={handleCollapseFriends}>
             <ListItemText><strong>MIS AMIGOS</strong></ListItemText>
+            {openFriends ? <FaChevronDown /> : <FaChevronUp />}
           </MenuItem>
           <Divider sx={{ opacity: 1 }} />
           <Collapse in={openFriends} timeout="auto" unmountOnExit>
