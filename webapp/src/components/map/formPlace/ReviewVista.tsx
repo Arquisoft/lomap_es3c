c0d7c0bfc8260ef5bd6695f2d6e1287e996ea916
elpla <elplayerhd@hotmail.es>
2023-04-27 19:11:23 +0200

    Visualizando la puntuación en las reviews

--- webapp/src/components/map/formPlace/ReviewVista.tsx
+++ webapp/src/components/map/formPlace/ReviewVista.tsx
@@ -1,8 +1,4 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { Box, Collapse } from '@mui/material';
-import React, { useState } from 'react';
-import SliderMapPlace from './SliderMapPlace';
+import React from 'react';
 
 export interface ReviewProps {
     reviews: any;
@@ -13,12 +9,14 @@ function ReviewVista(props:ReviewProps){
     if(props.reviews !== undefined){
         return (
             <div className='text-center'>
-              {props.reviews.map((review: { id: React.Key | null | undefined; author: { identifier: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; reviewBody: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; reviewRating: { rating: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; datePublished: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
-                <div key={review.id}>
-                  <h3>{review.author.identifier}</h3>
-                  <p>{review.reviewBody}</p>
-                  <p>Rating: {review.reviewRating.rating}</p>
+              {props.reviews.map((review: { id: React.Key | null | undefined; author: { identifier: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; reviewBody: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; reviewRating: { ratingValue: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; datePublished: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
+                <div key={review.id} className='mb-3'>
+                  <div>
+                  <h4>{review.author.identifier}</h4>
                   <p>Published: {review.datePublished}</p>
+                  <p>Rating: {review.reviewRating.ratingValue}</p>
+                  <p>{review.reviewBody}</p>
+                  </div>
                 </div>
               ))}
             </div>);
