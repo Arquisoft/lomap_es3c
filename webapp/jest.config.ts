af954c17ac763d3c168ef9b4250eedf8a3eec665
elpla <elplayerhd@hotmail.es>
2023-04-30 23:08:58 +0200

    Prueba configuracion tests

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,9 +1,6 @@
 export default {
-    collectCoverage: true,
-    collectCoverageFrom: ["src/components/**/*.{tsx}"],
     setupFilesAfterEnv: ['./src/setupTests.ts'],
     transform: {
         "^.+\\.tsx?$": "ts-jest"
-    },
-    testTimeout:150000,
+    }
 }
