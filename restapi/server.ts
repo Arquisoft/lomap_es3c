2e8c3eb5961a38f1892e3481a1d69fe0ce4b7bba
elpla <elplayerhd@hotmail.es>
2023-05-02 20:45:01 +0200

    Modificando server.ts

--- restapi/server.ts
+++ restapi/server.ts
@@ -6,9 +6,12 @@ import api from "./api";
 
 const app: Application = express();
 const port: number = 5000;
-
+const https = require('https');
+const fs = require('fs');
 // BBDD Conf 2/6 - Require (Importar)
-const db = require("./src/db")
+const db = require("./src/db");
+
+const httpsPort = 5001;
 
 const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
@@ -17,6 +20,17 @@ app.use(cors());
 app.use(bp.json());
 
 app.use("/api", api)
+
+const options = {
+    key: fs.readFileSync(process.env.SSL_PRIVKEY),
+    cert: fs.readFileSync(process.env.SSL_CERT)
+  };
+
+https.createServer(options, app).listen(httpsPort, () => {
+    console.log(`Restapi server started on port ${httpsPort}`);
+  }).on("error", (error: Error) => {
+      console.error("Error occured: " + error.message);
+});
 
 app.listen(port, ():void => {
     console.log('Restapi listening on '+ port);
